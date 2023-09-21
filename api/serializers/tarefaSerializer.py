5847c81477a3fb86569aee22b9b94542b211e9f9
camiyuka <camila.yatabe@gmail.com>
2023-09-21 16:53:16 -0300

    feat(tarefa): tarefaSerializer

---
+++ api/serializers/tarefaSerializer.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from api.models.tarefaModels import TarefaModel
+
+class DisciplinaSerializer(serializers.ModelSerializer):
+     class Meta:
+        model = TarefaModel
+        fields = '__all__'  # Use a list or tuple, not parentheses
