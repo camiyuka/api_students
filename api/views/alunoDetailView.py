d1139837b9073df4fcdc766f912ebcb5a4718d2f
camiyuka <camila.yatabe@gmail.com>
2023-09-20 22:52:53 -0300

    feat(aluno): request in alunoDetailView

--- api/views/alunoDetailView.py
+++ api/views/alunoDetailView.py
@@ -7,11 +7,10 @@ from api.models.alunoModel import AlunoModel
 from api.serializers.alunoSerializer import AlunoSerializer 
 
 class AlunoDetailView(APIView):
-
     # detalhar| Retorna detalhes de um aluno específico com base no id
-    def get(self, id):
+    def get(self, request,pk):
         try:
-            alunos = AlunoModel.objects.get(pk=id)
+            alunos = AlunoModel.objects.get(pk=pk)
             serializer= AlunoSerializer(alunos)
             return Response(serializer.data, status=status.HTTP_200_OK)
         except AlunoModel.DoesNotExist:
@@ -20,21 +19,21 @@ class AlunoDetailView(APIView):
             return Response(str(error), status=status.HTTP_400_BAD_REQUEST)
 
     # atualizar |  permite a atualização dos detalhes de um aluno específico com base no id                            
-    def put(self, id):
-        try:
-            alunos=AlunoModel.objects.get(pk=id)
-            serializer = AlunoSerializer(alunos, data= alunos.data, partial=True)
-            if(serializer.is_valid()):
-                serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        except:
-               return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def put(self, request, pk):
+      #  try:
+        alunos=AlunoModel.objects.get(pk=pk)
+        serializer = AlunoSerializer(alunos, data= request.data, partial=True)
+        if(serializer.is_valid()):
+            serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+      #  except:
+           #    return Response(status=status.HTTP_400_BAD_REQUEST)
 
     # excluir | Permite a exclusão de um aluno específico com base no id. Além disso, todas as tarefas associadas a esse aluno devem ser excluídas ou desassociadas.      
 
-    def delete(self, id):
+    def delete(self, request, pk):
         try:
-            alunos = AlunoModel.objects.get(pk=id)
+            alunos = AlunoModel.objects.get(pk=pk)
             alunos.delete()
             return Response( "aluno deletado com sucesso!",status= status.HTTP_204_NO_CONTENT)
         except AlunoModel.DoesNotExist:
