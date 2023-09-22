2d63970c9f3d2334e5fa46b886b4ca2861e7d3e8
camiyuka <camila.yatabe@gmail.com>
2023-09-22 19:52:55 -0300

    docs: model's documentation

--- api/models/disciplinaModel.py
+++ api/models/disciplinaModel.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+# criação de uma disciplina com nome e descrição
 class DisciplinaModel(models.Model):
     nome= models.CharField(max_length=50)
     descricao=models.CharField(max_length=100)
--- api/models/tarefaModel.py
+++ api/models/tarefaModel.py
@@ -2,11 +2,15 @@ from django.db import models
 from api.models.alunoModel import AlunoModel
 from api.models.disciplinaModel import DisciplinaModel
 
+# criação de tarefa, com título, descrição e disciplina
 class TarefaModel(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField()
     data_entrega = models.DateField()
     concluida = models.BooleanField(default=False)
-    
+
+    # aluno está associado a tarefa, se aluno for excluído, essa tarefa também será
     aluno = models.ForeignKey(AlunoModel, on_delete=models.CASCADE)
+
+    # várias disciplinas estão associadas a essa tarefa
     disciplinas = models.ManyToManyField(DisciplinaModel)   
